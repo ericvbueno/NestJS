fca2f6ec7e2aaa176efb06d9ca54df9ca2c4cfd8
ericvbueno <ericv.bueno@gmail.com>
2022-06-28 23:14:13 -0300

    [feat] - Implementando o metodo atualizar

--- src/jogadores/jogadores.service.ts
+++ src/jogadores/jogadores.service.ts
@@ -10,7 +10,19 @@ export class JogadoresService {
     private readonly logger = new Logger(JogadoresService.name)
 
     async criarAtualizarJogador(criaJogadorDto: CriarJogadorDto): Promise<void> {
-        this.criar(criaJogadorDto);
+        const { email } = criaJogadorDto
+
+        const jogadorEncontrado = await this.jogadores.find(jogador => jogador.email === email)
+
+        if (jogadorEncontrado) {
+            await this.atualizar(jogadorEncontrado, criaJogadorDto)
+        } else {
+            this.criar(criaJogadorDto);
+        }
+    }
+
+    async consultarTodosJogadores(): Promise<Jogador[]> {
+        return await this.jogadores;
     }
 
     private criar(criaJogadorDto: CriarJogadorDto): void {
@@ -27,5 +39,10 @@ export class JogadoresService {
         }
         this.logger.log(`criaJogadorDto: ${JSON.stringify(jogador)}`);
         this.jogadores.push(jogador);
+    }
+
+    private atualizar(jogadorEncontrado: Jogador, criarJogadorDto: CriarJogadorDto): void {
+        const { nome } = criarJogadorDto
+        jogadorEncontrado.nome = nome;
     }
 }
