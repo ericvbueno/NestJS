bb56b536d4f6abfddd168913b5faf92fcb7a7043
ericvbueno <ericv.bueno@gmail.com>
2022-07-06 21:28:06 -0300

    [feat] - Criando a interface de conexão com o MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { JogadoresModule } from './jogadores/jogadores.module';
 
 @Module({
-  imports: [JogadoresModule],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://admin:oRq0cuImUfA6fSeG@atlascluster.r7vy31r.mongodb.net/smartranking?retryWrites=true&w=majority',
+    { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false }),
+    JogadoresModule
+  ],
   controllers: [],
   providers: [],
 })
