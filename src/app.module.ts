6e9c5f4b1cee679d817aa7c41e941e82e38cf67c
ericvbueno <ericv.bueno@gmail.com>
2022-07-07 22:19:44 -0300

    [fix] - Corrigindo o module de conexão

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { JogadoresModule } from './jogadores/jogadores.module';
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb+srv://admin:oRq0cuImUfA6fSeG@atlascluster.r7vy31r.mongodb.net/smartranking?retryWrites=true&w=majority',
-    { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false }),
+    { useNewUrlParser: true, useUnifiedTopology: true }),
     JogadoresModule
   ],
   controllers: [],
