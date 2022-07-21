6364a0197b28428c88073bae995ff02dc5a799ae
ericvbueno <ericv.bueno@gmail.com>
2022-07-20 22:41:46 -0300

    [feat] - Iniciando a implementação do componente de categorias

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { JogadoresModule } from './jogadores/jogadores.module';
+import { CategoriasModule } from './categorias/categorias.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb+srv://admin:oRq0cuImUfA6fSeG@atlascluster.r7vy31r.mongodb.net/smartranking?retryWrites=true&w=majority',
     { useNewUrlParser: true, useUnifiedTopology: true }),
-    JogadoresModule
+    JogadoresModule,
+    CategoriasModule
   ],
   controllers: [],
   providers: [],
