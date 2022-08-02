75768779a38f3e84f9dcca5e31900db2f025aad7
ericvbueno <ericv.bueno@gmail.com>
2022-08-01 21:32:44 -0300

    [feat] - Iniciando implementação do modulo desafios

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { JogadoresModule } from './jogadores/jogadores.module';
 import { CategoriasModule } from './categorias/categorias.module';
+import { DesafiosModule } from './desafios/desafios.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb+srv://admin:oRq0cuImUfA6fSeG@atlascluster.r7vy31r.mongodb.net/smartranking?retryWrites=true&w=majority',
     { useNewUrlParser: true, useUnifiedTopology: true }),
     JogadoresModule,
-    CategoriasModule
+    CategoriasModule,
+    DesafiosModule
   ],
   controllers: [],
   providers: [],
---
+++ src/desafios/desafios.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { JogadoresService } from 'src/jogadores/jogadores.service';
+import { CriarDesafioDto } from './dtos/criar-desafio.dto';
+import { Desafio } from './interfaces/desafio.interface';
+
+@Injectable()
+export class DesafiosService {
+
+    constructor(
+        @InjectModel('Desafio') private readonly desafioModel: Model<Desafio>,
+        private readonly jogadoresService: JogadoresService){}
+
+    async criarDesafio(criarDesafioDto: CriarDesafioDto): Promise<Desafio> {
+        return
+    }
+}
